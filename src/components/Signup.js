db17248aae01a064abb5c6c315f4c9631bfd9a50
tu1014 <tu1014@naver.com>
2022-06-09 15:50:17 +0900

    회원가입 썸네일 구현

---
+++ src/components/Signup.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "../App.css";
+import { Link } from "react-router-dom";
+
+const Signup = () => {
+
+    return (
+        <div className="signup-container">
+            <div className="signup-card">
+                <div className="signup-image" />
+                <div className="signup-info">
+                    <span>CJ 회원이 되시면 하나의 통합된 회원 ID와 비밀번호로</span><br></br>
+                    <span>CGV와 CJ ONE의 다양한 서비스를 이용하실 수 있습니다.</span>
+                </div>
+                <Link to="/signupForm">
+                    <div className="signup-button">
+                        <span>CGV + CJ ONE 통합회원 가입</span>
+                    </div>
+                </Link>
+            </div>
+        </div>
+
+    );
+}
+
+export default Signup;
---
+++ src/components/SignupForm.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../App.css";
+import { Link } from "react-router-dom";
+
+const SignupForm = () => {
+
+    return (
+        <div className="signupForm-container">
+            <div className="signupForm-header">
+                <h1>CGV + CJ ONE 통합회원 가입</h1>
+                <p>통합 아이디로  CJ ONE 브랜드 혜택도 받고! 포인트도 쌓고!</p>
+            </div>
+            <div className="signupForm-card">
+                <div className="signupForm-first-row">
+                    <div>
+                        <h3>회원 가입</h3>
+                    </div>
+                    <div>
+                        호야무
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default SignupForm;
