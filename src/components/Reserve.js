3c22915033b840b7f8e08b1677be99d176d84406
tu1014 <tu1014@naver.com>
2022-06-14 00:59:37 +0900

    tt:

--- src/components/Reserve.js
+++ src/components/Reserve.js
@@ -28,7 +28,7 @@ function Reserve() {
             </div>
             <div class="time-body">
             <div class="time" id="time">
-                    <span class="1층1관">
+                    <span id = "1층1관" class="1층1관" value="시간_1층1관">
                         <h2>1관 1층</h2>
                     <input type="radio"  onClick={(e) => timeClick(e)}  name="time_level" id="rd5" value="시간_1층1관1"/>
                     <label for="rd5" id="시간_1층1관1"></label><br/>
@@ -36,7 +36,7 @@ function Reserve() {
                     <label for="rd6" id="시간_1층1관2"></label><br/>
                     </span>
                     
-                    <span class="2층2관">
+                    <span id = "2층2관" class="2층2관" value="시간_2층2관">
                         <h2>2관 2층</h2>
                     <input type="radio"  onClick={(e) => timeClick(e)} name="time_level" id="rd7" value="시간_2층2관1"/>
                     <label for="rd7" id="시간_2층2관1"></label><br/>
@@ -48,35 +48,39 @@ function Reserve() {
         </div>
     </div>
 
-    {/* <link rel="stylesheet" href="http://img.cgv.co.kr/CGV_RIA/Ticket/Common/css/2022/0512/FORM_TYPE/reservation_tnb.css"/> */}
+
 
 <div id="ticket_tnb" class="tnb_container ">
     <div class="tnb step2">
         
         <div class="info movie">
-            <span class="movie_poster"><img src="http://img.cgv.co.kr/Movie/Thumbnail/Poster/000082/82120/82120_185.JPG" alt="영화 포스터"/></span>
+            <span class="movie_poster"><img id="image" src="http://m.blog.naver.com/joanishappy/220754485187" alt=""/></span>
             <div class="row movie_title colspan2">
-                <span class="data letter-spacing-min ellipsis-line2"><a href="http://www.cgv.co.kr/movies/detail-view/?midx=82120" target="_blank" onmousedown="javascript:logClick('SUMMARY/영화상세보기');" title="탑건-매버릭">탑건-매버릭</a></span>
+                <span class="data letter-spacing-min ellipsis-line2">
+                    <a id = "title" title="탑건-매버릭"></a></span>
             </div>
-            <div class="row movie_type">
-                <span class="data ellipsis-line1" title="2D(프리미어 상영)">2D(프리미어 상영)</span>
-            </div>
+            
             <div class="row movie_rating">
-                <span class="data" title="12세 관람가">12세 관람가</span>
+                <span id = "rating" class="data" title="12세 관람가"></span>
             </div>
         </div>
         <div class="info theater">
             <div class="row name">
                 <span class="header">극장</span>
-                <span class="data letter-spacing-min ellipsis-line1"><a target="_blank" onmousedown="javascript:logClick('SUMMARY/극장상세보기');" href="http://www.cgv.co.kr/theaters/?theaterCode=0057" title="CGV 미아">CGV 구미</a></span>
+                <span class="data letter-spacing-min ellipsis-line1">
+                    <a  title="CGV 미아">CGV 구미</a></span>
             </div>
             <div class="row date">
-                <span class="header">일시</span>
-                <span class="data" title="2022.6.19(일) 15:20">2022.6.19(일) 15:20</span>
+                <span class="header">날짜</span>
+                <span class="data" title="2022.6.19(일) 15:20">2022.6.16(목)</span>
+            </div>
+            <div class="row date">
+                <span class="header">시간</span>
+                <span class="data" id = "time3" title="2022.6.19(일) 15:20"></span>
             </div>
             <div class="row screen_">
                 <span class="header">상영관</span>
-                <span class="data" title="1관 9층">1관 1층</span>
+                <span class="data" id = "상영관" title="1관 9층"></span>
             </div>
             <div class="row number">
                 <span class="header">인원</span>
@@ -111,6 +115,7 @@ function Reserve() {
 
 var 시간_1층1관 = [];
 var 시간_2층2관 = [];
+var rating; //시청 등급
 
 
 
@@ -130,9 +135,15 @@ function movieClick(e){
 	// }
 
     const time1 = document.getElementById('시간_1층1관1');
+    const time1_rdo = document.getElementById('rd5');
     const time2 = document.getElementById('시간_1층1관2');
+    const time2_rdo = document.getElementById('rd6');
     const time3 = document.getElementById('시간_2층2관1');
+    const time3_rdo = document.getElementById('rd7');
     const time4 = document.getElementById('시간_2층2관2');
+    const time4_rdo = document.getElementById('rd8');
+    const title = document.getElementById('title');
+    const rate = document.getElementById('rating');
 
     var 브로커_시간_1층1관 = ["13:00", "15:30"];
     var 브로커_시간_2층2관 = ["10:15", "18:00"];
@@ -149,33 +160,58 @@ function movieClick(e){
     if(val == "브로커"){
         시간_1층1관 = 브로커_시간_1층1관;
         시간_2층2관 = 브로커_시간_2층2관;
+        rating = "12세 관람가";
+        document.getElementById("image").src = "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000085/85829/85829_185.JPG";
+        
 
     } else if(val == "범죄도시2"){
         시간_1층1관 = 범죄도시2_시간_1층1관;
         시간_2층2관 = 범죄도시2_시간_2층2관;
+        rating = "15세 관람가";
+        document.getElementById("image").src="http://img.cgv.co.kr/Movie/Thumbnail/Poster/000085/85813/85813_185.JPG";
     } else if(val == "마녀2"){
         시간_1층1관 = 마녀2_시간_1층1관;
         시간_2층2관 = 마녀2_시간_2층2관;
+        rating = "15세 관람가";
+        document.getElementById("image").src = "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000085/85871/85871_185.JPG";
+
     } else if(val == "탑건-매버릭"){
         시간_1층1관 = 탑건_매버릭_시간_1층1관;
         시간_2층2관 = 탑건_매버릭_시간_2층2관;
+        rating = "12세 관람가";
+        document.getElementById("image").src = "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000082/82120/82120_185.JPG";
     }
 
 
     time1.innerHTML = 시간_1층1관[0];
-        time2.innerHTML = 시간_1층1관[1];
-        time3.innerHTML = 시간_2층2관[0];
-        time4.innerHTML = 시간_2층2관[1];
-
+    time1_rdo.setAttribute('value', 시간_1층1관[0]);
+    time2.innerHTML = 시간_1층1관[1];
+    time2_rdo.setAttribute('value', 시간_1층1관[1]);
+    time3.innerHTML = 시간_2층2관[0];
+    time3_rdo.setAttribute('value', 시간_2층2관[0]);
+    time4.innerHTML = 시간_2층2관[1];
+    time4_rdo.setAttribute('value', 시간_2층2관[1]);
+    title.innerHTML = val;
+    rate.innerHTML = rating;
         
     
 }
 
 function timeClick(e){
-    console.log(e.target.value);
-    var con = document.getElementById("tnb_step_btn_right");
+    const time3 = document.getElementById('time3');
+    const 상영관 = document.getElementById('상영관');
     
+
+    console.log(time3);
+    
+    console.log("e : ", e.target);
+    console.log("val : ", e.target.value);
+
+    var con = document.getElementById("tnb_step_btn_right");
 		con.className = "btn-right_on";
+
+    time3.innerHTML = e.target.value;
+    상영관.innerHTML = e.target.parentElement.id;
     
 
 }
