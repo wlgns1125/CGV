462825d3d65924fe107fe3909dc3c7d50219e35d
park <durms1125@naver.com>
2022-06-14 01:06:34 +0900

    dfsdf

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,52 @@
 import "../css/Header.css";
 import { useState } from "react";
-function openNav() {
-    let sidebar = document.querySelector(".side_nav");
-    sidebar.classList.add("display_sidebar");
-}
-
-function closeNav() {
-    let sidebar = document.querySelector(".side_nav");
-    sidebar.classList.remove("display_sidebar");
-}
-
+import { FaAngleDown } from "react-icons/fa";
 
 function Header() {
+    const [isOpen, setMenu] = useState(false);  // 메뉴의 초기값을 false로 설정
+  
+    const toggleMenu = () => {
+          setMenu(isOpen => !isOpen); // on,off 개념 boolean
+      }
+    
   return(
       <div>
-     <div class="side_nav">
-        <span class="closebtn">&times;</span>
-        <a href="#">옵션</a>
-        <a href="#">가전</a>
-        <a href="#">가구</a>
-        <a href="#">캠핑용품</a>
-   
-    <span class="openbtn">&#9776; open</span>
-    </div>
-
-
-
+        
+      <div className="container">
+        <input id="dropdown" type="checkbox" />
+        <label className="dropdownLabel" for="dropdown">
+          <span className="menuBar">&#9776;</span>
+        </label>
+        <div className="content">
+        <ul class = "menubar">
+                        <li>
+                            <a href="">
+                            <img src="https://img.cgv.co.kr/R2014/images/common/ico/loginPassword.png" alt="로그인"></img>
+                            <span>로그인</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                            <img src="https://img.cgv.co.kr/R2014/images/common/ico/loginJoin.png" alt="회원가입"></img>
+                            <span>회원가입</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                            <img src="https://img.cgv.co.kr/R2014/images/common/ico/loginMember.png" alt="MY CGV"></img>
+                            <span>MY CGV</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="   ">
+                            <img src="https://img.cgv.co.kr/R2014/images/common/ico/loginMember.png" alt="고객샌터"></img>
+                            <span>고객센터</span>
+                            </a>
+                        </li>
+                    </ul>
+        </div>
+      </div>
+  
       <div class ="header">
               <div class = "header_content">
               <div class = "contents">
@@ -86,5 +107,7 @@ function Header() {
       </div>
   );
 }
+
+
 
 export default Header;
